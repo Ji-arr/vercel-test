e3b3516a0093aa4242d6808142f56f1f94aac878
Ji-arr <gfor508@gmail.com>
2024-10-18 08:39:13 +0800

    remove pwa

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import VitePWA from "vite-plugin-pwa";
 import { ViteSSR } from "vite-plugin-ssr";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    ViteSSR(),
-    VitePWA(), // Optional: for PWA features
-  ],
+  plugins: [react(), ViteSSR()],
 });
